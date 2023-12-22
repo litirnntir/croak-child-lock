6485af304d59a9ff1a6035147ce9c284699d8c84
salnyed <agorbuncova2004@gmail.com>
2023-12-23 01:20:34 +0400

    Added multiprocessing test and main window

---
+++ MainWindow.py
@@ -0,0 +1,18 @@
+from PyQt6.QtGui import QPixmap, QPalette, QBrush
+from PyQt6.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui()
+
+    def ui(self):
+        self.setObjectName("Croak")
+        self.setFixedSize(840, 580)
+
+        background_image = "/Users/aleksandragorbuncova/PycharmProjects/croak-child-lock/images/background.png"
+        pix = QPixmap(background_image)
+        pal = QPalette()
+        pal.setBrush(self.backgroundRole(), QBrush(pix))
+        self.setPalette(pal)
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import multiprocessing
 import sys
 
+import telebot
 from PyQt6.QtWidgets import QApplication
+from sett import TOKEN
 
 from MainWindow import MainWindow
+
+bot = telebot.TeleBot(TOKEN)
+
+
+def run_bot():
+    bot.polling()
 
 
 def run_window():
@@ -12,5 +21,18 @@ def run_window():
     sys.exit(app.exec())
 
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привет")
+
+
 if __name__ == '__main__':
-    run_window()
+    multiprocessing.freeze_support()
+    time_process = multiprocessing.Process(target=run_window)
+    bot_process = multiprocessing.Process(target=run_bot)
+
+    bot_process.start()
+    time_process.start()
+
+    bot_process.join()
+    time_process.join()
