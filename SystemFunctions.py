d074fdbe0eb43bd0388525f3d2405e30e15efeea
salnyed <agorbuncova2004@gmail.com>
2023-12-23 03:40:53 +0400

    Added typehint and docstring

--- SystemFunctions.py
+++ SystemFunctions.py
@@ -1,11 +1,6 @@
-import base64
 import json
 
 from cryptography.fernet import Fernet
-
-# key = Fernet.generate_key()
-# with open("key.key", "wb") as file:
-#     file.write(key)
 
 with open("key.key", "rb") as file:
     key = file.read()
@@ -13,7 +8,11 @@ with open("key.key", "rb") as file:
 fernet = Fernet(key)
 
 
-def encrypt_json(filename):
+def encrypt_json(filename: str) -> None:
+    """Шифрует содержимое JSON-файла с помощью ключа fernet.
+
+    :param filename: имя JSON-файла для шифрования
+    """
     with open(filename, "rb") as file:
         data = file.read()
     encrypted_data = fernet.encrypt(data)
@@ -21,14 +20,25 @@ def encrypt_json(filename):
         file.write(encrypted_data)
 
 
-def decrypt_json(filename):
+def decrypt_json(filename: str) -> str:
+    """Расшифровывает содержимое JSON-файла с помощью ключа fernet.
+
+    :param filename: имя JSON-файла для расшифровки
+    :return: расшифрованное содержимое JSON-файла в виде строки
+    """
     with open(filename, "rb") as file:
         encrypted_data = file.read()
     decrypted_data = fernet.decrypt(encrypted_data)
     return decrypted_data.decode("utf-8")
 
 
-def update_json(filename, key, value):
+def update_json(filename: str, key: str, value: any) -> None:
+    """Обновляет значение по ключу в JSON-файле и шифрует его с помощью ключа fernet.
+
+    :param filename: имя JSON-файла для обновления
+    :param key: ключ, по которому нужно обновить значение
+    :param value: новое значение для ключа
+    """
     data = json.loads(decrypt_json(filename))
     data[key] = value
     # dict -> json -> bytes
@@ -36,14 +46,8 @@ def update_json(filename, key, value):
     bytes_obj = data.encode('utf-8')
     encrypted_data = fernet.encrypt(bytes_obj)
     print(data)
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
+    with open(filename, "wb") as f:
+        f.write(encrypted_data)
 
 
 update_json("test.json", "total", 10)
-
-# encrypt_json("test.json")
-
-# a = json.loads(decrypt_json("test.json"))
-# with open("test.json", "w") as f:
-#     json.dump(a, f)
