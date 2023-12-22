d552f852d75f5fb5f567727ef6359fb1ae8bc89f
salnyed <agorbuncova2004@gmail.com>
2023-12-23 03:34:19 +0400

    Added update json function

--- SystemFunctions.py
+++ SystemFunctions.py
@@ -3,9 +3,9 @@ import json
 
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
-with open("key.key", "wb") as file:
-    file.write(key)
+# key = Fernet.generate_key()
+# with open("key.key", "wb") as file:
+#     file.write(key)
 
 with open("key.key", "rb") as file:
     key = file.read()
@@ -28,7 +28,22 @@ def decrypt_json(filename):
     return decrypted_data.decode("utf-8")
 
 
-encrypt_json("test.json")
-a = json.loads(decrypt_json("test.json"))
-with open("test.json", "w") as f:
-    json.dump(a, f)
+def update_json(filename, key, value):
+    data = json.loads(decrypt_json(filename))
+    data[key] = value
+    # dict -> json -> bytes
+    data = json.dumps(data)
+    bytes_obj = data.encode('utf-8')
+    encrypted_data = fernet.encrypt(bytes_obj)
+    print(data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+
+
+update_json("test.json", "total", 10)
+
+# encrypt_json("test.json")
+
+# a = json.loads(decrypt_json("test.json"))
+# with open("test.json", "w") as f:
+#     json.dump(a, f)
